ad9ab819bb55041db075e14867f9417137ac8cd1
asoe72 <asoe72@gmail.com>
2021-07-18 11:22:30 +0900

    함수명 camelCase로 통일

--- src/bookbind.js
+++ src/bookbind.js
@@ -7,15 +7,15 @@ const md_it = require("markdown-it");
 ///@param[in]	pathfile_toc		
 exports.bind = function(path_out, path_in, pathfile_toc)
 {
-	const toc = read_toc(pathfile_toc);
-	bind_html_with_toc(path_out, path_in, toc);
+	const toc = readToc(pathfile_toc);
+	bindHtmlWithToc(path_out, path_in, toc);
 }
 
 
 ///@param[in]	path_out
 ///@param[in]	path_in
 ///@param[in]	toc
-function bind_html_with_toc(path_out, path_in, toc)
+function bindHtmlWithToc(path_out, path_in, toc)
 {
 	var pathfile_out = path.join(path_out, "book.html");
 	var str_all = "";
@@ -27,7 +27,7 @@ function bind_html_with_toc(path_out, path_in, toc)
 
 		var str_html = fs.readFileSync(pathname_html, 'utf8');
 
-		str_html = replace_html_hd(str_html, item.level);
+		str_html = replaceHtml_hd(str_html, item.level);
 
 		str_all += str_html;
 	}
@@ -45,20 +45,20 @@ function bind_html_with_toc(path_out, path_in, toc)
 
 
 ///@param[in]	pathfile_toc
-function read_toc(pathfile_toc)
+function readToc(pathfile_toc)
 {
 	const str_toc = fs.readFileSync(pathfile_toc, 'utf8');
 	const md = md_it();
 	const arr_token = md.parse(str_toc);
 
-	const toc = arr_item_from_arr_token(arr_token);
+	const toc = arrItemFromArrToken(arr_token);
 	return toc;
 }
 
 
 ///@param[in]	arr_token
 // @return		arr_item	간략화한 배열
-function arr_item_from_arr_token(arr_token)
+function arrItemFromArrToken(arr_token)
 {
 	var arr_item = [];
 
@@ -72,7 +72,7 @@ function arr_item_from_arr_token(arr_token)
 		const arr_tc = token.children;
 		const link_md = arr_tc[0].attrs[0][1];
 		item.link = link_md.replace(".md", ".html");
-		item.link = remove_folder_trailing_dot(item.link);
+		item.link = removeFolderTrailingDot(item.link);
 		item.title = arr_tc[1].content;
 		item.level = (token.level-1)/2;
 		
@@ -87,7 +87,7 @@ function arr_item_from_arr_token(arr_token)
 
 ///@param[in]	str	"flowcontrol-subprogram/3.2./README.md"
 ///@return		"flowcontrol-subprogram/3.2/README.md"
-function remove_folder_trailing_dot(str)
+function removeFolderTrailingDot(str)
 {
     var re = /\.\//g;
 	return str.replace(re, '/');
@@ -97,7 +97,7 @@ function remove_folder_trailing_dot(str)
 ///@param[in]	str_body	"<h1>introduction</h1>"
 ///@param[in]	level		1~6
 ///@return		"<h2>introduction</h2>"
-function replace_html_hd(str_body, level)
+function replaceHtml_hd(str_body, level)
 {
 	if(level==1) return str_body;
 
