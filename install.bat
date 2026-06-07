d7661bb7a44b64d9bce93b698196b211d0bed966
최원혁 <asoe72@hd.com>
2026-06-08 08:59:41 +0900

    install.bat 보완

--- install.bat
+++ install.bat
@@ -1,4 +1,9 @@
+taskkill /f /im node.exe
+rmdir /s /q node_modules package-lock.json
+set PUPPETEER_SKIP_DOWNLOAD=true
+
 call npm install
+call npm install -D concurrently
 pushd frontend
 call npm install
 popd
